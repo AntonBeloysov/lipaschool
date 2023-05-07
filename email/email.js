6ee00ccea5db43e2a66a7f5c5c2c1894c3326ce6
Bordach <132823002+AntonBeloysov@users.noreply.github.com>
2023-05-07 15:20:29 +0300

    Update email.js

--- email/email.js
+++ email/email.js
@@ -2,10 +2,10 @@
 
 function send() {
     Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "robingud765@gmail.com",
-        Password : "BA85E04FE3E74B6A07947AAA46EF204887E7",
-        To : 'robingud765@gmail.com',
+        Host : "",
+        Username : "",
+        Password : "",
+        To : '',
         From : document.getElementById('email').value,
         Subject : document.getElementById('number').value,
         Body : document.getElementById('message').value + ' ' + document.getElementById('select').value
